e7da3e53855905c0aaa3af41473292269f7570df
Edward Cruz <sagecreations.ec@gmail.com>
2025-09-15 13:12:18 -0500

    validation is in place, running tests on parse.c

--- src/main.c
+++ src/main.c
@@ -7,31 +7,47 @@
 #include "file.h"
 #include "parse.h"
 
+// TODO: add -r (remove an employee from th database)
+// TODO: add -e (edit the amount of hours an existing employee has)
+
 void print_usage(char *argv[]) {
 	printf("Usage: %s -n -f <database file>\n", argv[0]);
 	printf("\t-n - create new database file\n");
 	printf("\t-f - (required) path to database\n");
+	printf("\t-a - add an employee to the specified database\n");
+	printf("\t-l - list all emplyoyees in the specified database\n");
 }
 
 int main(int argc, char *argv[]) { 
-	
 	char *filepath = NULL;
+	char *addstring = NULL;
+	bool list = false;
 	bool newfile = false;
 	int c;
+
+	int dbfd = -1;
+	struct dbheader_t *dbhdr = NULL;
+	struct employee_t *employees = NULL;
 	
-	while ((c != getopt(argc, argv, "nf:")) != -1) {
+	while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
 		switch (c) {
-		case 'n':
-			newfile = true;
-			break;
-		case 'f':
-			filepath = optarg;
-			break;
-		case '?':
-			printf("Unkown option -%c\n", c);
-			break;
-		default:
-			return -1;
+			case 'n':
+				newfile = true;
+				break;
+			case 'f':
+				filepath = optarg;
+				break;
+			case 'a':
+				addstring = optarg;
+				break;
+			case 'l':
+				list = true;
+				break;
+			case '?':
+				printf("Unkown option -%c\n", c);
+				break;
+			default:
+				return -1;
 		}
 	}
 	
@@ -42,8 +58,44 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
-	printf("Newfile: %d\n", newfile);
-	printf("Filepath: %s\n", filepath);
+	if (newfile) {
+		dbfd = create_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to create database file\n");
+			return -1;
+		}
+
+		if (create_db_header(&dbhdr) == STATUS_ERROR) {
+			printf("Failed to create database header\n");
+			return -1;
+		}
+	} else {
+		dbfd = open_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to open database file\n");
+			return -1;
+		}
+
+		if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to validate database header\n");
+			return -1;
+		}
+	}
+
+	if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS) {
+		printf("Failed to read emplyees");
+		return -1;	
+	}
+
+	if (addstring) {
+		add_employee(dbhdr, &employees, addstring);
+	}
+
+	if (list) {
+		list_employees(dbhdr, employees);
+	}
+
+	output_file(dbfd, dbhdr, employees);
 
 	return 0;
 }
